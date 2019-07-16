edcac69b61db69a8e3cb8a9089e25616abd3d063
Hưng Phan <phandinhhungvp2001@gmail.com>
2019-07-16 09:10:17 +0700

    Add Lotus Devices Official

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,4 @@
-for device in $(python vendor/lotus/tools/get_official_devices.py)
+for combo in $(curl -s https://raw.githubusercontent.com/LotusOS/android_vendor_lotus/pie/lotus.devices | sed -e 's/#.*$//' | awk '{printf "lotus_%s-%s\n", $1, $2}')
 do
-for var in user userdebug; do
-add_lunch_combo lotus_$device-$var
-done
+    add_lunch_combo $combo
 done
